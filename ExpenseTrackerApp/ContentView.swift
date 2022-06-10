327daf590c3df66b95f84eca03bbfef451b6c6be
Aidan Terlizzi <act5@rice.edu>
2022-06-10 11:59:50 -0400

    Initial Commit

---
+++ ExpenseTrackerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpenseTrackerApp
+//
+//  Created by Aidan Terlizzi on 6/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Aidan Terlizzi on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
